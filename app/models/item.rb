76ade9d20386194f540d3ca01a7736a9d0fde2f9
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-11-13 17:05:13 +0900

    create genre  model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
 
   belongs_to :user
+  belongs_to :genre
   mount_uploader :image, ImageUploader
   mount_uploader :video, VideoUploader
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_062056) do
+ActiveRecord::Schema.define(version: 2019_11_13_071533) do
+
+  create_table "genres", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
