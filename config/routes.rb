680eac578f95a7ceadd5fbb60f5f34b2aab39830
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-11-14 10:51:13 +0900

    add routesing of search action

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root to: "users#index"
   
   resources :users
-  resources :items
-  
+  resources :items do
+    collection do
+      get 'search'
+    end
+  end
 end
