626a5a81d31f4a1b7cfb17a0622322f8566c7c79
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-11-17 13:37:33 +0900

    add js function

---
+++ app/assets/javascripts/items.js
@@ -0,0 +1,18 @@
+$( function() {
+
+
+  $(".body__box__regisration").on("mouseover",function(){
+  $(this).css
+  ({"background-color": "#F1940B",
+    "font-weight": "bold"
+   });
+  })
+
+.on("mouseout", function(){
+  $(this).css({
+    "background-color": "blue", 
+    "font-weight": "normal"});
+  })
+
+
+})
