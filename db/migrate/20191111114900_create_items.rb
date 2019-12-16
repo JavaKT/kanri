b7cc79f1ec223e1cbaa4aa821c2ef9513722a1ea
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-12-16 18:34:50 +0900

    edit migration file

--- db/migrate/20191111114900_create_items.rb
+++ db/migrate/20191111114900_create_items.rb
@@ -1,14 +1,17 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
-      t.string  :name, null: false
-      t.integer :price 
-      t.integer :bugdet
-      t.text :color
-      t.datetime :on_air, default: -> { 'NOW()' }
-      t.text :comment
+      
       t.references :user, null: false ,foreign_key: true
+      t.string :name, null: false
+      t.string :video 
+      t.string :image
+      t.integer :price, null: false
+      t.integer :bugdet, null: false
+      t.text :color, null: false
+      t.datetime :on_air, default: -> { 'NOW()' }
+      t.text :description
+      t.integer :category_id, null: false
       t.timestamps
       
     end
--- db/migrate/20191113062056_add_video_to_item.rb
+++
@@ -1,5 +0,0 @@
-class AddVideoToItem < ActiveRecord::Migration[5.2]
-  def change
-    add_column :items, :video, :string
-  end
-end
