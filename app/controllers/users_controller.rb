b64a2b44523e97b83a0d96574e575336fbf31266
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-12-18 18:53:51 +0900

    edit user model migration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,17 @@ class UsersController < ApplicationController
   def index
     
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  private
+  params.require(:user).permit(:email,:password,:password_confirmation,
+                                :username,:department,:image,:introduciton,:contact)
+                                
 end
--- db/migrate/20191111095519_add_username_to_users.rb
+++ db/migrate/20191111095519_add_username_to_users.rb
@@ -1,5 +1,9 @@
 class AddUsernameToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :nickname, :string
+    add_column :users, :username, :string
+    add_column :users, :image, :string
+    add_column :users, :department, :string
+    add_column :users, :introduction, :string
+    add_column :users, :contact, :string
   end
 end
