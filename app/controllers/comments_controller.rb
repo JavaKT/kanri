81a96813b637c394c917d0bac960692ea5fe157a
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-12-30 13:28:26 +0900

    add gem of gon

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.create(text: comment_params[:text], item_id: comment_params[:item_id], user_id: current_user.id)
-    respond_to do |format|
+    @comment = Comment.create(comment_params)
+    respond_to do|format|
       format.json
     end
   end
@@ -20,7 +20,7 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.permit(:text, :item_id)
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
   def set_item
