9d49bd989306a0f5e645a9781c7d4b621c0b609d
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2019-12-30 18:59:29 +0900

    add time_zone Tokyo

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Kanri
     config.i18n.default_locale = :ja 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
+    config.time_zone = 'Tokyo'
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
