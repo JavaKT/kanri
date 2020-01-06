569007cb6151254be923ccb504a41352dc9c551a
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2020-01-06 19:56:23 +0900

    add js

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -1,6 +1,27 @@
 $(document).on('turbolinks:load',function() {
+
+  function imageHTML(){
+    var html = `<input type="file" name="item[images_attributes][0][image]" id="item_images_attributes_0_image1">`
+    return html;
+  }
+
+  function image1HTML(){
+    var htmml = `<input type="file" name="item[images_attributes][0][image]" id="item_images_attributes_0_image2">`
+    return htmml;
+  }
+
+
   $('#item_images_attributes_0_image').on("change", function() { 
-    var form = $(this).closest('form'); 
-    form.append(`input type="file" name="item[images_attributes][0][image]" id="item_images_attributes_0_image"`); 
+    var html = imageHTML()
+    $('.field_image').append(html);
+    $(this).val('');
   }); 
+
+  $('#item_images_attributes_0_image1').on("change", function() { 
+    console.log("繰り返す")
+
+    var htmml = image1HTML()
+    $('.field_image').append(htmml);
+  }); 
+
 })
