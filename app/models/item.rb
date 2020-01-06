6c7876870638f64c9369ac79848fcd6b51e6f575
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2020-01-06 12:43:20 +0900

    add model  of image

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,9 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_many :comments
-  mount_uploader :image, ImageUploader
+  has_many :images
+  accepts_nested_attributes_for :images
+
   mount_uploader :video, VideoUploader
 
   enum category:{ "美容":1,
