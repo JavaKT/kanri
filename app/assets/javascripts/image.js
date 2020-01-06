bc5f55bed4e24d325c4fc16ee9969d49132fb3ff
KOTA ITO <56817448+JavaKT@users.noreply.github.com>
2020-01-06 21:43:55 +0900

    add js

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -1,27 +1,5 @@
-$(document).on('turbolinks:load',function() {
-
-  function imageHTML(){
-    var html = `<input type="file" name="item[images_attributes][0][image]" id="item_images_attributes_0_image1">`
-    return html;
-  }
-
-  function image1HTML(){
-    var htmml = `<input type="file" name="item[images_attributes][0][image]" id="item_images_attributes_0_image2">`
-    return htmml;
-  }
-
-
-  $('#item_images_attributes_0_image').on("change", function() { 
-    var html = imageHTML()
+  $(document).on("change", "#item_images_attributes_0_image", function () {
+  var html = `<input type="file" name="item[images_attributes][0][image]" id="item_images_attributes_0_image">`
     $('.field_image').append(html);
-    $(this).val('');
+    $('#item_images_attributes_0_image').prop("disabled", true);
   }); 
-
-  $('#item_images_attributes_0_image1').on("change", function() { 
-    console.log("繰り返す")
-
-    var htmml = image1HTML()
-    $('.field_image').append(htmml);
-  }); 
-
-})
